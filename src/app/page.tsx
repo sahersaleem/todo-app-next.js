98c996d3c4683a0b99bea43ec56bd330945b27b3
Muhammad Saad <160317082+sahersaleem@users.noreply.github.com>
2024-11-22 16:51:08 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,142 @@
-import Image from "next/image";
+"use client";
 
+import { ChangeEvent, useEffect, useState } from "react";
+import { FaEdit} from "react-icons/fa";
+import { FaDeleteLeft } from "react-icons/fa6";
+
+interface ITodo {
+  id: number;
+  todo: string;
+}
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tasks, setTasks] = useState<ITodo[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+  const [mounted, setisMounted] = useState<boolean>(false);
+  const [editTask, setEditTask] = useState<string>("");
+  const [editTaskId, setEditTaskId] = useState<number | null>(null);
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newTask.trim()) {
+      setTasks((prevTask) => [
+        ...prevTask,
+        {
+          todo: newTask,
+          id: Date.now(),
+        },
+      ]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      setNewTask("");
+    } else {
+      alert("Please enter task");
+    }
+  };
+
+  const handleDelete = (id: number) => {
+    const filterTodos = tasks!.filter((task) => task.id !== id);
+    setTasks(filterTodos);
+  };
+
+  const handleEdit = (task: string, id: number) => {
+    setEditTask(task);
+    setEditTaskId(id);
+  };
+
+  const handleUpdateTask = () => {
+    if (editTask.trim() !== "") {
+      setTasks(
+        tasks.map((item) =>
+          item.id === editTaskId ? { ...item, todo: editTask } : item
+        )
+      );
+    }
+    setEditTask("");
+    setEditTaskId(null);
+  };
+
+  useEffect(() => {
+    setisMounted(true);
+    const savedtask = localStorage.getItem("todos");
+    console.log("task get");
+    if (savedtask) {
+      const parsedTask = JSON.parse(savedtask);
+
+      if (Array.isArray(parsedTask)) {
+        setTasks(parsedTask);
+      }
+    }
+  }, []);
+  useEffect(() => {
+    if (mounted) {
+      localStorage.setItem("todos", JSON.stringify(tasks));
+      console.log("task stored");
+    }
+  }, [tasks, mounted]);
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-auto bg-slate-200 text-black px-20 py-7 rounded-xl">
+        <h1 className="text-3xl mb-4 font-bold">Todo App</h1>
+
+        <form onSubmit={handleSubmit} className="flex gap-x-4">
+          <input
+          className="px-2 py-1 rounded-sm border-[4px] border-black"
+            name="todo"
+            value={newTask}
+            onChange={(e) => {
+              setNewTask(e.target.value);
+            }}
+            placeholder="Enter todo"
+          />{" "}
+          <button className="bg-black text-white px-4" type="submit">Add</button>
+        </form>
+        <div>
+          {tasks?.map((item) => {
+            return (
+              <div
+                className="flex gap-4 justify-center items-center"
+                key={item.id}
+              >
+                {item.id === editTaskId ? (
+                  <input
+                    value={editTask}
+                    onChange={(e) => {
+                      setEditTask(e.target.value);
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key == "Enter") {
+                        handleUpdateTask();
+                      }
+                    }}
+                    placeholder="edit task"
+                  />
+                ) : (
+                  <p className="text-lg ml-10 text-wrap w-1/2">{item.todo}</p>
+                )}
+<div className="flex justify-around items-center  w-full py-5">
+               
+                <div className="flex gap-x-4 text-xl">
+                <button
+                  onClick={() => {
+                    handleDelete(item.id);
+                  }}
+                >
+                  <FaDeleteLeft /> 
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    handleEdit(item.todo, item.id);
+                  }}
+                >
+                  <FaEdit /> 
+                </button>
+                </div>
+              </div>
+              </div>
+            );
+          })}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
